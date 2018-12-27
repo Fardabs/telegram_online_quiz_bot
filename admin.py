1a6ae738731c66f107e05ae68e5176538c6f9311
mohamadaref <aliismyimam@gmail.com>
2018-12-27 09:50:10 +0330

    defining function 'save_questions_to_db'

--- admin.py
+++ admin.py
@@ -4,8 +4,30 @@ from pandas import read_excel
 import xlrd
 import pandas
 
-class admin():
-    data = read_excel('data/questions_and_choices.xlsx')
-    print(data.columns[2])
+# data = xlrd.open_workbook('data/questions_and_choices.xlsx')
+# row_num = 0
+# col_num = 0
+# sh1 = data.sheet_by_index(0)
+# print(sh1.ncols)
 
-    # print(data.head())
+class admin():
+    data = xlrd.open_workbook('data/questions_and_choices.xlsx')
+
+
+    #a function for importing admin sorted questions
+    @classmethod
+    def import_questions(cls):
+        row_num = 1
+        col_num = 0
+        sh1 = cls.data.sheet_by_index(0)
+        for col_num in range(0,sh1.ncols):
+            if(sh1.cell(row_num,col_num).value != 'none'):
+                print(sh1.cell(row_num,col_num).value)
+            else:
+                pass
+            
+
+
+
+
+admin.import_questions()
